06ebf51c0681b757a65d06960a42d073cceab8df
Renat Nabiev <renatnabiew@MacBook-Pro.local>
2025-05-28 01:03:44 +0300

    Fix routing for login and register pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { CssBaseline, ThemeProvider } from '@mui/material';
-import { theme } from './theme';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import MainPage from './pages/MainPage';
@@ -45,134 +43,120 @@ const StudentRoute = ({ children }: { children: React.ReactNode }) => {
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Router>
-        <Routes>
-          {/* Public routes */}
-          <Route
-            path='/login'
-            element={
-              <PublicRoute>
-                <LoginPage />
-              </PublicRoute>
-            }
-          />
-          <Route
-            path='/register'
-            element={
-              <PublicRoute>
-                <RegisterPage />
-              </PublicRoute>
-            }
-          />
+    <Routes>
+      {/* Public routes */}
+      <Route
+        path='/login'
+        element={
+          <PublicRoute>
+            <LoginPage />
+          </PublicRoute>
+        }
+      />
+      <Route
+        path='/register'
+        element={
+          <PublicRoute>
+            <RegisterPage />
+          </PublicRoute>
+        }
+      />
 
-          {/* Protected routes */}
-          <Route
-            path='/dashboard'
-            element={
-              <ProtectedRoute>
-                <MainPage />
-              </ProtectedRoute>
-            }
-          />
+      {/* Protected routes */}
+      <Route
+        path='/dashboard'
+        element={
+          <ProtectedRoute>
+            <MainPage />
+          </ProtectedRoute>
+        }
+      />
 
-          <Route
-            path='/schedule'
-            element={
-              <ProtectedRoute>
-                <SchedulePage />
-              </ProtectedRoute>
-            }
-          />
+      <Route
+        path='/schedule'
+        element={
+          <ProtectedRoute>
+            <SchedulePage />
+          </ProtectedRoute>
+        }
+      />
 
-          <Route
-            path='/schedule/day/:day'
-            element={
-              <ProtectedRoute>
-                <SchedulePage />
-              </ProtectedRoute>
-            }
-          />
+      <Route
+        path='/schedule/day/:day'
+        element={
+          <ProtectedRoute>
+            <SchedulePage />
+          </ProtectedRoute>
+        }
+      />
 
-          <Route
-            path='/profile'
-            element={
-              <ProtectedRoute>
-                <ProfilePage />
-              </ProtectedRoute>
-            }
-          />
+      <Route
+        path='/profile'
+        element={
+          <ProtectedRoute>
+            <ProfilePage />
+          </ProtectedRoute>
+        }
+      />
 
-          <Route
-            path='/search'
-            element={
-              <StudentRoute>
-                <TutorSearchPage />
-              </StudentRoute>
-            }
-          />
+      <Route
+        path='/search'
+        element={
+          <StudentRoute>
+            <TutorSearchPage />
+          </StudentRoute>
+        }
+      />
 
-          <Route
-            path='/students'
-            element={
-              <TeacherRoute>
-                <StudentsPage />
-              </TeacherRoute>
-            }
-          />
+      <Route
+        path='/students'
+        element={
+          <TeacherRoute>
+            <StudentsPage />
+          </TeacherRoute>
+        }
+      />
 
-          <Route
-            path='/lessons'
-            element={
-              <TeacherRoute>
-                <LessonsPage />
-              </TeacherRoute>
-            }
-          />
+      <Route
+        path='/lessons'
+        element={
+          <TeacherRoute>
+            <LessonsPage />
+          </TeacherRoute>
+        }
+      />
 
-          <Route
-            path='/lessons/:lessonId'
-            element={
-              <ProtectedRoute>
-                <LessonDetailsPage />
-              </ProtectedRoute>
-            }
-          />
+      <Route
+        path='/lessons/:lessonId'
+        element={
+          <ProtectedRoute>
+            <LessonDetailsPage />
+          </ProtectedRoute>
+        }
+      />
 
-          <Route
-            path='/create-lesson'
-            element={
-              <TeacherRoute>
-                <CreateLessonPage />
-              </TeacherRoute>
-            }
-          />
+      <Route
+        path='/create-lesson'
+        element={
+          <TeacherRoute>
+            <CreateLessonPage />
+          </TeacherRoute>
+        }
+      />
 
-          {/* Redirect root to dashboard */}
-          <Route
-            path='/'
-            element={
-              <PublicRoute>
-                <HomePage />
-              </PublicRoute>
-            }
-          />
+      {/* Root route */}
+      <Route
+        path='/'
+        element={
+          <PublicRoute>
+            <HomePage />
+          </PublicRoute>
+        }
+      />
 
-          <Route
-            path='/dashboard'
-            element={
-              <ProtectedRoute>
-                <MainPage />
-              </ProtectedRoute>
-            }
-          />
-
-          {/* Catch all route - redirect to dashboard */}
-          <Route path='*' element={<Navigate to='/dashboard' replace />} />
-        </Routes>
-      </Router>
-    </ThemeProvider>
+      {/* Catch all route - redirect to dashboard */}
+      <Route path='*' element={<Navigate to='/dashboard' replace />} />
+    </Routes>
   );
 }
 
